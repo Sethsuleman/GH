3c7dbeaab5c32d22d34570207bd78e9619db6ab4
Sethsuleman <sethsuleman09@gmail.com>
2026-06-27 12:07:19 +0500

    Online store

---
+++ java.js
@@ -0,0 +1,29 @@
+function addtocart1(){
+    document.getElementById("message1").innerText=
+    "Product Added To Cart!"
+}
+
+function addtocart2(){
+    document.getElementById("message2").innerText=
+    "Product Added To Cart!"
+}
+
+function addtocart3(){
+    document.getElementById("message3").innerText=
+    "Product Added To Cart!"
+}
+
+function addtocar4t(){
+    document.getElementById("message4").innerText=
+    "Product Added To Cart!"
+}
+
+function addtocart5(){
+    document.getElementById("message5").innerText=
+    "Product Added To Cart!"
+}
+
+function addtocart6(){
+    document.getElementById("message6").innerText=
+    "Product Added To Cart!"
+}
